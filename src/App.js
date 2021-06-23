966d9b847421a7de5be62b20345c9688b9364fb7
dukanynot <anthony@okocha.net>
2021-06-22 23:44:05 -0400

    tweak 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Welcome Master Tony</code> 
+          <code>Welcome Master Tony</code> 
         </p>
         <a
           className="App-link"
